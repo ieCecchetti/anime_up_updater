b4aba425632f1eadcf466d87dc0145a7ce495a20
Enrico Cecchetti <enrico.cecchetti@kcitylabs.fr>
2024-04-06 16:58:50 +0200

    store the daily scratch result to avoid the ban when I locally test the algo

--- anime_up_updater/functions/scraper.py
+++ anime_up_updater/functions/scraper.py
@@ -3,36 +3,73 @@ from anime_up_updater.utils.logger import logger
 from anime_up_updater.config.constants import graphql_query
 import json
 from anime_up_updater.functions.times import get_previous_monday_timestamp, get_next_sunday_timestamp, timestamp_to_datetime
+import os
+from datetime import datetime
+import time
 
 # Set the timeout value (in seconds)
-timeout = 2000
+timeout = 10000
 
 
-def scrape_all():
-    has_next = True
-    page_num = 1
-    airing_anime = []
-    while has_next:
-        has_next, data_page = scrape_single_page(page=page_num)
-        airing_anime.extend(data_page)
-        page_num += 1
+def scrape_all(force_download=False):
+    week_start = get_previous_monday_timestamp()
+    week_end = get_next_sunday_timestamp()
+
+    actual_day_start = datetime.fromtimestamp(
+        week_start).strftime('%d')
+    actual_day_end = datetime.fromtimestamp(
+        week_end).strftime('%d')
+    actual_month = datetime.now().month
+    actual_year = datetime.now().year
+
+    #  check if already available
+    stored_file_path = f"./anime_up_updater/res/airing_anime_list_{actual_day_start}-{
+        actual_day_end}_{actual_month}_{actual_year}.json"
+    if not os.path.exists(stored_file_path):
+        already_download = False
+    else:
+        print(f"The airing list already exists at: {stored_file_path}.")
+        already_download = True
+
+    if force_download or not already_download:
+        has_next = True
+        page_num = 1
+        airing_anime = []
+        while has_next:
+            has_next, data_page = scrape_single_page(
+                w_start=week_start,
+                w_end=week_end,
+                page=page_num)
+            airing_anime.extend(data_page)
+            # Sleep for 5 seconds
+            time.sleep(5)
+            page_num += 1
+        print("Storing for future usage")
+        # Store the JSON response in the output file
+        with open(stored_file_path, "w") as json_file:
+            json.dump(data_page, json_file, indent=4)
+    else:
+        # Open the JSON file and load its contents into a variable
+        with open(stored_file_path, "r") as json_file:
+            airing_anime = json.load(json_file)
 
     return airing_anime
 
 
-def scrape_single_page(page, store_result=False):
+def scrape_single_page(w_start, w_end, page):
+    logger.debug(f"Querying anime for start_date: {
+        timestamp_to_datetime(w_start)}")
+    logger.debug(f"Querying anime for end_date: {
+        timestamp_to_datetime(w_end)}")
+    logger.debug(f"Querying page: {page}")
+    logger.debug(f"---------------------------------")
+
     # GraphQL query and variables
     graphql_variables = {
         "weekStart": get_previous_monday_timestamp(),
         "weekEnd": get_next_sunday_timestamp(),
         "page": page
     }
-    logger.debug(f"Querying anime for start_date: {
-        timestamp_to_datetime(graphql_variables['weekStart'])}")
-    logger.debug(f"Querying anime for end_date: {
-        timestamp_to_datetime(graphql_variables['weekEnd'])}")
-    logger.debug(f"Querying page: {graphql_variables['page']}")
-    logger.debug(f"---------------------------------")
 
     # Prepare the request headers
     headers = {
@@ -55,10 +92,6 @@ def scrape_single_page(page, store_result=False):
         response_json = response.json()
         has_next = response_json['data']['Page']['pageInfo']['hasNextPage']
         data_page = response_json['data']['Page']['airingSchedules']
-        if store_result:
-            # Store the JSON response in the output file
-            with open(f"../res/airing_anime_list_{graphql_variables['weekStart']}_{page}.json", "w") as json_file:
-                json.dump(data_page, json_file, indent=4)
     else:
         # Print an error message if the request failed
         raise RuntimeError(f"Failed to fetch data: {response.status_code}")
--- anime_up_updater/script.py
+++ anime_up_updater/script.py
@@ -2,15 +2,33 @@ import argparse
 from anime_up_updater.utils.logger import logger
 from anime_up_updater.functions import scraper
 import json
+import ptvsd
+
+
+ptvsd.enable_attach(address=('localhost', 5678))
+ptvsd.wait_for_attach()
+
+def store_in_db(db_conn_str, data):
+    logger.info(
+        f"Storing #{len(data)} data in mongo database at: {db_conn_str}")
+    pass
+
+
+def store_as_json(out_path, data):
+    logger.info(f"Storing data in File at: {out_path}")
+    # Store the JSON response in the output file
+    with open(out_path, "w") as json_file:
+        json.dump(data, json_file, indent=4)
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Description of your script')
-    parser.add_argument('-s', '--db_conn_str',
-                        help='Path to the db where to store the data')
-    parser.add_argument(
-        '-o', '--output-file', help='Path to the output file to store the data')
+    print("Welcome to anime parser")
 
+    parser = argparse.ArgumentParser(description='Description of your script')
+    parser.add_argument('--db_conn_str', type=str,
+                        help='Path to the db where to store the data')
+    parser.add_argument('--output-file', type=str,
+                        help='Path to the output file to store the data')
     # Parse the arguments
     args = parser.parse_args()
 
@@ -19,12 +37,13 @@ def main():
 
     # Check which options are selected
     if args.db_conn_str:
-        logger.info(f"Storing data in database at: {args.db_conn_str}")
+        store_in_db(args.db_conn_str, airing_anime)
     if args.output_file:
-        logger.info(f"Storing data in File at: {args.output_file}")
-        # Store the JSON response in the output file
-        with open(args.output_file, "w") as json_file:
-            json.dump(airing_anime, json_file, indent=4)
+        store_as_json(args.output_file, airing_anime)
+    else:
+        print("No action selected for storing the obtained data..")
+
+    print("Process finished successfully")
 
 
 if __name__ == "__main__":
