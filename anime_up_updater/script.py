428cabea90239e78776ba695257b725561a63517
Enrico Cecchetti <enrico.cecchetti@kcitylabs.fr>
2024-04-06 18:36:16 +0200

    add mongo db to store airing_anime

--- anime_up_updater/script.py
+++ anime_up_updater/script.py
@@ -2,18 +2,37 @@ import argparse
 from anime_up_updater.utils.logger import logger
 from anime_up_updater.functions import scraper
 import json
+import os
+from pymongo import MongoClient
 
-def store_in_db(db_conn_str, data):
+
+def store_in_db(data, db_conn_str, database_name, collection_name="airing_anime"):
+    database_name = database_name if database_name else "anime_up"
+    collection_name = collection_name if collection_name else "airing_anime"
     logger.info(
-        f"Storing #{len(data)} data in mongo database at: {db_conn_str}")
-    pass
+        f"Storing data in mongo collection `{collection_name}` at: {db_conn_str}/{database_name}")
+    # Connect to MongoDB
+    client = MongoClient(db_conn_str)
+    # Access a database
+    db = client[database_name]
+    # Access a collection (create it if it doesn't exist)
+    collection = db[collection_name]
+    # Insert the array of dictionaries into the collection
+    collection.insert_many(data)
+    # Close the connection
+    client.close()
+    logger.info(
+        f"Stored #{len(data)} data into mongo")
 
 
 def store_as_json(out_path, data):
-    logger.info(f"Storing data in File at: {out_path}")
+    if ".json" not in out_path:
+        out_path = os.path.join(
+            out_path, "airing_anime.json")
     # Store the JSON response in the output file
     with open(out_path, "w") as json_file:
         json.dump(data, json_file, indent=4)
+    logger.info(f"File stored at: {out_path}")
 
 
 def main():
@@ -21,7 +40,11 @@ def main():
 
     parser = argparse.ArgumentParser(description='Description of your script')
     parser.add_argument('--db-conn-str', type=str,
-                        help='Path to the db where to store the data')
+                        help='Path to the mongo db where to store the data')
+    parser.add_argument('--database', type=str,
+                        help='Database name in case of db storage')
+    parser.add_argument('--collection', type=str,
+                        help='Collection name in case of db storage')
     parser.add_argument('--output-file', type=str,
                         help='Path to the output file to store the data')
     parser.add_argument(
@@ -40,7 +63,8 @@ def main():
 
     # Check which options are selected
     if args.db_conn_str:
-        store_in_db(args.db_conn_str, airing_anime)
+        store_in_db(airing_anime, args.db_conn_str,
+                    args.database, args.collection)
     if args.output_file:
         store_as_json(args.output_file, airing_anime)
     else:
